c8a6afe444167c1501f80327132877340317d28e
Your Name <you@example.com>
2019-12-25 22:09:02 +0900

    responseEntity

--- src/main/java/testGitHub/io/Controller/TestController.java
+++ src/main/java/testGitHub/io/Controller/TestController.java
@@ -2,6 +2,8 @@ package testGitHub.io.Controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -9,7 +11,11 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import testGitHub.io.Entity.TestVo;
 
 @Controller
 public class TestController {
@@ -17,10 +23,32 @@ public class TestController {
 	@Autowired
 	ResourceLoader resourceloader;
 	
+	TestVo testVO;
+	
+	
+	@ModelAttribute
+	private TestVo test(TestVo vo){
+		testVO = vo;
+		return testVO;
+	}
+	
 	@RequestMapping("/exSubUrl/downloadtest_real.do")
-	public ResponseEntity<Resource> test() throws IOException{
+	public ResponseEntity<Resource> test(@RequestParam HashMap asMap,
+										 @ModelAttribute HashMap modelMap,
+										 @ModelAttribute TestVo vo
+			) throws Exception{
 		Resource resource = resourceloader.getResource("classpath:"+"/image/ch.jpg");
 		File file = resource.getFile();
+		
+		System.out.println(vo.getInputFirst());
+		System.out.println(vo.getInputSecond());
+		System.out.println(testVO.getInputFirst());
+		System.out.println(testVO.getInputSecond());
+		
+		System.out.println((String)modelMap.get("inputFirst"));
+		System.out.println((String)modelMap.get("inputSecond"));
+		System.out.println((String)asMap.get("inputFirst"));
+		System.out.println((String)asMap.get("inputSecond"));
 		
 		/*apache tika core*/
 		Tika tika = new Tika();
---
+++ src/main/java/testGitHub/io/Entity/TestVo.java
@@ -0,0 +1,20 @@
+package testGitHub.io.Entity;
+
+public class TestVo {
+
+	private String inputFirst;
+	private String inputSecond;
+	
+	public String getInputFirst() {
+		return inputFirst;
+	}
+	public void setInputFirst(String inputFirst) {
+		this.inputFirst = inputFirst;
+	}
+	public String getInputSecond() {
+		return inputSecond;
+	}
+	public void setInputSecond(String inputSecond) {
+		this.inputSecond = inputSecond;
+	}
+}
